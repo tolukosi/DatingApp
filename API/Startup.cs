65545e7b69028bdda7cefdbc92e267bab04f7f05
tolukosi <tolukosi@gmail.com>
2021-06-29 19:21:34 -0600

    Jun 29

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,6 +38,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            services.AddCors();
 
         }
 
@@ -54,6 +55,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
