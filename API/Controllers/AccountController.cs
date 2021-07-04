a4f32a78f759c2980aec33170b3200c247e6f55a
tolukosi <tolukosi@gmail.com>
2021-07-03 21:08:05 -0600

    Sat 03 07

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -37,9 +37,27 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+            if (user == null) return Unauthorized("Invalid Username");
+            
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var ComputedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < ComputedHash.Length; i++)
+            {
+                if (ComputedHash[i] != user.PasswordHash[i]) return Unauthorized("Wrong Password");
+            }
+
+            return user;
+        }
         private async Task<bool> UserExist(string username)
         {
-            return await _context.Users.AnyAsync(ECKeyXmlFormat => ECKeyXmlFormat.UserName == username.ToLower());
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
 
    
