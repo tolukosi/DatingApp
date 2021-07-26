68b51e2bac74c6d45a35efe24a90be9a32135118
tolukosi <tolukosi@gmail.com>
2021-07-25 18:25:29 -0600

    Async Error

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import {Observable} from "rxjs";
+import {User} from "../_models/user";
 
 @Component({
   selector: 'app-nav',
@@ -8,24 +10,24 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {}
-  loggedIn: boolean = false;
+  currentUser$: Observable<User>;
 
   constructor( private accountService: AccountService) { }
 
   ngOnInit(): void {
+  //  this.getCurrentUser();
+    this.currentUser$ = this.accountService.currentUser$;
   }
 
   login () {
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-      this.loggedIn=true;
     }, error => {
       console.log(error);
     })
   }
 
   logout() {
-    this.loggedIn = false;
+    this.accountService.logout();
   }
-
 }
