a33f483883914d67fd23e3b48db046924531a8d1
tolukosi <tolukosi@gmail.com>
2021-08-13 15:02:51 -0600

    109

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {map} from "rxjs/operators";
@@ -8,7 +9,7 @@ import {ReplaySubject} from "rxjs";
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
 
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
